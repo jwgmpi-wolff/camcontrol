39e2f9a4ac5f757d879804992494f64ef0a4aa21
jwgmpi-wolff <150378616+jerrywolff_microsoft@users.noreply.github.com>
2026-09-22 13:11:36 -0700

    Daemonize native camera streamer

--- src/camera_bridge/oncam/overlay/camcontrol-streamer.c
+++ src/camera_bridge/oncam/overlay/camcontrol-streamer.c
@@ -14,6 +14,17 @@
 
 static volatile sig_atomic_t running = 1;
 
+static int daemonize(void) {
+    pid_t pid = fork();
+    if (pid < 0) return -1;
+    if (pid > 0) return 1;
+    if (setsid() < 0) return -1;
+    pid = fork();
+    if (pid < 0) return -1;
+    if (pid > 0) _exit(0);
+    return 0;
+}
+
 static void stop_streamer(int signal_number) {
     (void)signal_number;
     running = 0;
@@ -109,6 +120,7 @@ failed:
 int main(void) {
     char endpoint[256], host[128], port[16], camera_id[128], camera_key[128], interval_text[16];
     unsigned int interval = 1;
+    if (daemonize() != 0) return 0;
     signal(SIGTERM, stop_streamer);
     signal(SIGINT, stop_streamer);
     while (running) {
